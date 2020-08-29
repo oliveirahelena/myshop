9f6ca7df68a62c1dc77378dde62baf6bb845e213
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-08-29 18:50:43 -0300

    export order to pdf and send email

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,11 @@
 import braintree
 from django.shortcuts import render, redirect, get_object_or_404
 from orders.models import Order
+import weasyprint
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from io import BytesIO
 
 
 def payment_process(request):
@@ -24,6 +29,25 @@ def payment_process(request):
             # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            # create invoice e-mail
+            subject = f'My Shop - Invoice no. {order.id}'
+            message = 'Please, find attached the invoice for your recent purchase.'
+            email = EmailMessage(subject,
+                                 message,
+                                 'admin@myshop.com',
+                                 [order.email])
+            # generate PDF
+            html = render_to_string('orders/order/pdf.html', {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out,
+                                                   stylesheets=stylesheets)
+            # attach PDF file
+            email.attach(f'order_{order.id}.pdf',
+                         out.getvalue(),
+                         'application/pdf')
+            # send e-mail
+            email.send()
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
